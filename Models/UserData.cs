16b7df6031bde38d9506b1ec0d262b9e2939b587
mrDivyshah <105691678+mrDivyshah@users.noreply.github.com>
2025-02-07 19:16:41 +0530

    new local

---
+++ Models/UserData.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceBuddy.Models
+{
+    public class UserData
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmailConfirmed { get; set; } = string.Empty;
+        public string PasswordReset { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string TokenExpires { get; set; } = string.Empty;
+        public string Created { get; set; } = string.Empty;
+        public string Updated { get; set; } = string.Empty;
+        public string Deleted { get; set; } = string.Empty;
+        public int Active { get; set; } = 1;
+        public int Blocked { get; set; } = 0;
+        public int Removed { get; set; } = 0;
+        public int RemovedExpires { get; set; } = 0;
+        public string RemovedReason { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zip { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ServiceBuddy.Data;
+
 namespace ServiceBuddy
 {
     public class Program
@@ -5,9 +8,11 @@ namespace ServiceBuddy
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
             // Configure the HTTP request pipeline.
